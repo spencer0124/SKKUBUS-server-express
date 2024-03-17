ce0cec640f661e79952ffe3804813e5591966b2f
spencer0124 <spencer0124@naver.com>
2024-03-17 18:30:08 +0900

    add: ad detail and statistics

--- route/ad/ad.js
+++ route/ad/ad.js
@@ -4,8 +4,10 @@ const router = express.Router();
 // Define your function properly
 function getAdList() {
   return {
-    image: "https://i.imgur.com/K2JsUO6.png",
+    image: "https://i.imgur.com/A620zXw.png",
     link: "instagram://user?username=skkubus.app",
+    text: "스꾸버스 재학생 다운로드 41% 돌파 🔥",
+    image2: "https://i.imgur.com/niiVxOH.png",
   };
 }
 
@@ -13,6 +15,51 @@ function getAdList() {
 router.get("/v1/addetail", async (req, res) => {
   const AdListData = getAdList(); // Corrected function call
   res.json(AdListData);
+});
+
+// menu1
+var menu1_view = 0;
+
+router.get("/v1/statistics/menu1/view", async (req, res) => {
+  menu1_view++;
+  res.json({ count: menu1_view });
+});
+
+var menu1_click = 0;
+
+router.get("/v1/statistics/menu1/click", async (req, res) => {
+  menu1_click++;
+  res.json({ count: menu1_click });
+});
+
+// menu2
+var menu2_view = 0;
+
+router.get("/v1/statistics/menu2/view", async (req, res) => {
+  menu2_view++;
+  res.json({ count: menu2_view });
+});
+
+var menu2_click = 0;
+
+router.get("/v1/statistics/menu2/click", async (req, res) => {
+  menu2_click++;
+  res.json({ count: menu2_click });
+});
+
+// menu3
+var menu3_view = 0;
+
+router.get("/v1/statistics/menu3/view", async (req, res) => {
+  menu3_view++;
+  res.json({ count: menu3_view });
+});
+
+var menu3_click = 0;
+
+router.get("/v1/statistics/menu3/click", async (req, res) => {
+  menu3_click++;
+  res.json({ count: menu3_click });
 });
 
 // Export the router with the attached route

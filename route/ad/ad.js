83440ff990f4f20ec0d8fe9a1019ed99600a0009
spencer0124 <spencer0124@naver.com>
2024-04-11 11:01:20 +0900

    notice: delete subs loading ad

--- route/ad/ad.js
+++ route/ad/ad.js
@@ -5,9 +5,7 @@ const { getData } = require("../poll/poll.js");
 // Define your function properly
 function getAdList() {
   return {
-    // 성대방송국
-    image: "https://i.imgur.com/zSrKJRO.jpeg",
-    // image: "https://i.imgur.com/A620zXw.png",
+    image: "https://i.imgur.com/A620zXw.png",
     // image: "https://i.imgur.com/yOcuOY8.png",
     image2: "https://i.imgur.com/Qd9bciY.jpeg",
     // image2: "https://i.imgur.com/niiVxOH.png",
